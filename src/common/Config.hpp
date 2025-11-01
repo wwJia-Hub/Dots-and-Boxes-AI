e79edd79f915e65371d375c32661ae735e2e619f
wwJia <wwJia-Hub@users.noreply.github.com>
2025-11-02 00:57:57 +0800

    Refactor Config class constructor and operator overloads for clarity
    - Removed unnecessary default constructor and whitespace in the Config class.
    - Streamlined the operator overloads for better readability and maintainability.

--- src/common/Config.hpp
+++ src/common/Config.hpp
@@ -7,14 +7,11 @@ static constexpr int BoardSize = 6;
 #define V(Class)         \
   public:                \
   Class() = default;     \
-                         \
   Class(int v) : v(v) {  \
   }                      \
-                         \
   void operator++(int) { \
     v++;                 \
   }                      \
-                         \
   operator int() const { \
     return v;            \
   }                      \
