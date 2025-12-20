d2b2a5c1a5432008df65df6e1f0ddbe090d1801b
wwJia <wwJia-Hub@users.noreply.github.com>
2025-12-20 15:47:07 +0800

    Enhance random number generation with RandomN function
    - Introduced a new inline function RandomN to simplify random number generation within a specified range, improving code clarity and maintainability.
    - Updated RandomChoice to utilize RandomN, streamlining the selection process from a data collection while ensuring proper assertions are in place.

--- src/common/Random.hpp
+++ src/common/Random.hpp
@@ -1,16 +1,23 @@
 #pragma once
 
+#include <cassert>
 #include <random>
+
+inline int
+RandomN(int N) {
+  assert(N > 0);
+  if (N == 1) {
+    return 0;
+  }
+  thread_local std::mt19937 rng(std::random_device{}());
+  std::uniform_int_distribution dist(0, N - 1);
+  return dist(rng);
+}
 
 template <class T>
 const auto&
 RandomChoice(const T& data) {
   assert(!data.Empty());
-  if (data.Size() == 1) {
-    return data.At(0);
-  }
-  thread_local std::mt19937 rng(std::random_device{}());
-  std::uniform_int_distribution dist(0, data.Size() - 1);
 
-  return data.At(dist(rng));
+  return data.At(RandomN(data.Size()));
 }
