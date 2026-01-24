671d35baaaa609fcc7b2cd3f527acf88a777db78
wwJia <wwJia-Hub@users.noreply.github.com>
2026-01-24 11:01:12 +0800

    refactor: initialize random_device separately for improved clarity in RandomChoice

--- src/common/Random.hpp
+++ src/common/Random.hpp
@@ -8,7 +8,8 @@ RandomChoice(const T& data) {
     return data.At(0);
   }
 
-  thread_local std::mt19937 rng(std::random_device{}());
+  thread_local std::random_device rd;
+  thread_local std::mt19937 rng(rd());
   std::uniform_int_distribution<size_t> dist(0, data.Size() - 1);
   return data.At(dist(rng));
 }
