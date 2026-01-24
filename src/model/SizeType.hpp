0fd340a10caa647fad0244f79ce53657fa1ef896
wwJia <wwJia-Hub@users.noreply.github.com>
2026-01-24 16:56:26 +0800

    refactor: simplify Int template

--- src/model/SizeType.hpp
+++ src/model/SizeType.hpp
@@ -2,12 +2,13 @@
 
 #include <cassert>
 #include <cstddef>
+#include <cstdint>
 #include <limits>
 #include <type_traits>
 
 // clang-format off
-template <int64_t BoardSize>
-using Int = std::conditional_t<2 * BoardSize * (BoardSize + 1) <= std::numeric_limits<int8_t>::max(), int8_t,
-  std::conditional_t<2 * BoardSize * (BoardSize + 1) <= std::numeric_limits<int16_t>::max(), int16_t, 
-  std::conditional_t<2 * BoardSize * (BoardSize + 1) <= std::numeric_limits<int32_t>::max(), int32_t, int64_t>>>;
+template <int64_t BoardSize, int64_t Max = 2 * BoardSize * (BoardSize + 1)>
+using Int = std::conditional_t<Max <= std::numeric_limits<int8_t>::max(), int8_t,
+  std::conditional_t<Max <= std::numeric_limits<int16_t>::max(), int16_t, 
+  std::conditional_t<Max <= std::numeric_limits<int32_t>::max(), int32_t, int64_t>>>;
 // clang-format on
