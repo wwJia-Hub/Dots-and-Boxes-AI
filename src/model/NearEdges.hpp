aa125100e2c3ec6918c160a35396ee91a268fd1d
wwJia <wwJia-Hub@users.noreply.github.com>
2026-01-27 00:07:35 +0800

    refactor(NearEdges): extract edge calculation logic to separate method
    The edge calculation logic is moved from the constructor to a new GetNearEdges method to improve code organization and maintainability

--- src/model/NearEdges.hpp
+++ src/model/NearEdges.hpp
@@ -19,25 +19,35 @@ class NearEdgesMapper {
   private:
   Array<Array<Edge<BoardSize>, 4>, Box<BoardSize>::Max> BoxNearEdges;
 
+  static Array<Edge<BoardSize>, 4>
+  GetNearEdges(const Box<BoardSize> box);
+
   friend const Array<Edge<BoardSize>, 4>&
   NearEdges<BoardSize>(const Box<BoardSize> box);
 };
 
 template <int64_t BoardSize>
 NearEdgesMapper<BoardSize>::NearEdgesMapper() {
-  for (const SizeType<BoardSize> x : Iota(Box<BoardSize>::Size)) {
-    for (const SizeType<BoardSize> y : Iota(Box<BoardSize>::Size)) {
-      const Dot<BoardSize> topLeft(x, y);
-      const Dot<BoardSize> topRight(x + 1, y);
-      const Dot<BoardSize> bottomLeft(x, y + 1);
-      const Dot<BoardSize> bottomRight(x + 1, y + 1);
-      const Box<BoardSize> box(x, y);
-      BoxNearEdges.At(box.Value()).At(0) = Edge(topLeft, topRight);
-      BoxNearEdges.At(box.Value()).At(1) = Edge(topLeft, bottomLeft);
-      BoxNearEdges.At(box.Value()).At(2) = Edge(bottomLeft, bottomRight);
-      BoxNearEdges.At(box.Value()).At(3) = Edge(topRight, bottomRight);
-    }
+  for (const Box<BoardSize> box : Iota(Box<BoardSize>::Max)) {
+    BoxNearEdges.At(box.Value()) = GetNearEdges(box);
   }
+}
+
+template <int64_t BoardSize>
+Array<Edge<BoardSize>, 4>
+NearEdgesMapper<BoardSize>::GetNearEdges(const Box<BoardSize> box) {
+  Array<Edge<BoardSize>, 4> NearEdges;
+  const SizeType<BoardSize> x = box.X();
+  const SizeType<BoardSize> y = box.Y();
+  const Dot<BoardSize> topLeft(x, y);
+  const Dot<BoardSize> topRight(x + 1, y);
+  const Dot<BoardSize> bottomLeft(x, y + 1);
+  const Dot<BoardSize> bottomRight(x + 1, y + 1);
+  NearEdges.At(0) = Edge(topLeft, topRight);
+  NearEdges.At(1) = Edge(topLeft, bottomLeft);
+  NearEdges.At(2) = Edge(bottomLeft, bottomRight);
+  NearEdges.At(3) = Edge(topRight, bottomRight);
+  return NearEdges;
 }
 
 template <int64_t BoardSize>
