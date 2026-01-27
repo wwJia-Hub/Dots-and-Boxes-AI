42156d268162da9d4031637f45ffd83f5114c05f
wwJia <wwJia-Hub@users.noreply.github.com>
2026-01-27 13:49:03 +0800

    refactor(CommandParser): replace aggregate initialization with constructor
    Use constructor for Config class initialization to improve encapsulation and maintainability

--- frontend/CommandParser.hpp
+++ frontend/CommandParser.hpp
@@ -22,10 +22,16 @@ static constexpr const char* PlayerTypeOptionStrings[] = {
 
 class Config {
   public:
+  Config(int64_t boardSize, PlayerType player1Type, PlayerType player2Type);
+
   int64_t BoardSize;
   PlayerType Player1Type;
   PlayerType Player2Type;
 };
+
+inline Config::Config(int64_t boardSize, PlayerType player1Type, PlayerType player2Type)
+    : BoardSize(boardSize), Player1Type(player1Type), Player2Type(player2Type) {
+}
 
 class CommandParser {
   public:
@@ -68,11 +74,7 @@ CommandParser::Process(const QApplication& application) {
         GetPlayerTypeString(player1Type),
         GetPlayerTypeString(player2Type));
 
-  return Config{
-      .BoardSize = boardSize,
-      .Player1Type = player1Type,
-      .Player2Type = player2Type,
-  };
+  return Config(boardSize, player1Type, player2Type);
 }
 
 inline QCommandLineOption
