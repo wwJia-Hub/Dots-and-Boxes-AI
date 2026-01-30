514dec5a49617056f9e7cd5c4159f3e6e985240f
wwJia <wwJia-Hub@users.noreply.github.com>
2026-01-30 14:53:49 +0800

    refactor(ParallelSearchRobot): make template parameters configurable
    Move SubRobotSearchTime and SubRobotNumber from static constants to template parameters to allow more flexible configuration of the parallel search behavior

--- src/robot/ParallelSearchRobot.hpp
+++ src/robot/ParallelSearchRobot.hpp
@@ -4,12 +4,10 @@
 
 namespace dab::detail::robot {
 
-template <int64_t BoardSize>
+template <int64_t BoardSize,
+          int64_t SubRobotSearchTime = static_cast<int64_t>(Limits<Edge<BoardSize>>::Max) << 5,
+          int64_t SubRobotNumber = 64>
 class ParallelSearchRobot final : public Robot<BoardSize> {
-  static constexpr int64_t SubRobotSearchTime = static_cast<int64_t>(Limits<Edge<BoardSize>>::Max) << 5;
-  static constexpr int64_t SubRobotNumber = 64;
-  static constexpr int64_t SearchTime = SubRobotNumber * SubRobotSearchTime;
-
   using SubRobotType = MonteCarloRobot<BoardSize, SubRobotSearchTime>;
 
   public:
@@ -23,9 +21,10 @@ class ParallelSearchRobot final : public Robot<BoardSize> {
   SearchScoreMap<BoardSize> SearchResult;
 };
 
-template <int64_t BoardSize>
+template <int64_t BoardSize, int64_t SubRobotSearchTime, int64_t SubRobotNumber>
 Span<Edge<BoardSize>>
-ParallelSearchRobot<BoardSize>::BestCandidateEdges(const ScoreCountableBoard<BoardSize>& board) {
+ParallelSearchRobot<BoardSize, SubRobotSearchTime, SubRobotNumber>::BestCandidateEdges(
+    const ScoreCountableBoard<BoardSize>& board) {
   if (Span<Edge<BoardSize>> edges; SubRobots.Front().CanEarlyExit(board, edges)) {
     return edges;
   }
