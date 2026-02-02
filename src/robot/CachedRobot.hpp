052bba7d569f49a6054167ff4d4ba57d525a61ba
wwJia <wwJia-Hub@users.noreply.github.com>
2026-02-02 20:47:54 +0800

    refactor(robot): simplify cache implementation by replacing lru with vector
    The previous LRU cache implementation was unnecessarily complex for the use case. Replaced it with a simpler vector-based approach that directly stores board states and results. This improves maintainability while maintaining functionality.

--- src/robot/CachedRobot.hpp
+++ src/robot/CachedRobot.hpp
@@ -1,10 +1,9 @@
 #pragma once
 
 #include <cstddef>
+#include <cstdio>
 #include <mutex>
 #include <vector>
-#include <list>
-#include <unordered_map>
 
 #include "Board.hpp"
 #include "Common.hpp"
@@ -16,53 +15,41 @@ template <int64_t BoardSize>
 class RobotCacheEntry {
   public:
   RobotCacheEntry() = default;
-  RobotCacheEntry(const std::vector<Edge<BoardSize>>& result)
-      : Result(result) {
+  RobotCacheEntry(const BasicBoard<BoardSize>& board, const std::vector<Edge<BoardSize>>& result)
+      : Board(board), Result(result) {
   }
 
+  BasicBoard<BoardSize> Board;
   std::vector<Edge<BoardSize>> Result;
-  size_t Hash;
 };
 
-template <int64_t BoardSize, size_t Cap = 1 << 10>
+template <int64_t BoardSize>
 class RobotCache {
   public:
-  RobotCache() = default;
+  RobotCache() {
+    Entrise.reserve(Edge<BoardSize>::Max);
+  }
 
   void
-  Find(size_t hash, Span<Edge<BoardSize>>& result) {
-    std::lock_guard lock(Mutex);
-    auto it = CacheMap.find(hash);
-    if (it != CacheMap.end()) {
-      LRUList.splice(LRUList.begin(), LRUList, it->second);
-      result = Span(it->second->Result.data(), it->second->Result.data() + it->second->Result.size());
+  Find(const BasicBoard<BoardSize>& board, Span<Edge<BoardSize>>& result) {
+    for (size_t i = 0; i < Entrise.size(); i++) {
+      RobotCacheEntry<BoardSize>& entry = Entrise[i];
+      if (entry.Board == board) {
+        result = Span(entry.Result.data(), entry.Result.data() + entry.Result.size());
+      }
     }
   }
 
   void
-  Add(size_t hash, const Span<Edge<BoardSize>>& result) {
+  Add(const BasicBoard<BoardSize>& board, const Span<Edge<BoardSize>>& result) {
     std::vector<Edge<BoardSize>> resultDump(result.Begin(), result.End());
     std::lock_guard lock(Mutex);
-    
-    auto it = CacheMap.find(hash);
-    if (it != CacheMap.end()) {
-      LRUList.splice(LRUList.begin(), LRUList, it->second);
-      it->second->Result = resultDump;
-    } else {
-      if (LRUList.size() >= Cap) {
-        CacheMap.erase(LRUList.back().Hash);
-        LRUList.pop_back();
-      }
-      LRUList.emplace_front(resultDump);
-      LRUList.front().Hash = hash;
-      CacheMap[hash] = LRUList.begin();
-    }
+    Entrise.emplace_back(board, resultDump);
   }
 
   private:
   std::mutex Mutex;
-  std::list<RobotCacheEntry<BoardSize>> LRUList;
-  std::unordered_map<size_t, typename std::list<RobotCacheEntry<BoardSize>>::iterator> CacheMap;
+  std::vector<RobotCacheEntry<BoardSize>> Entrise;
 };
 
 template <int64_t BoardSize, typename SubRobotType, size_t HashSize = 1 << 14>
@@ -82,14 +69,13 @@ class CachedRobot : public Robot<BoardSize> {
 template <int64_t BoardSize, typename SubRobotType, size_t HashSize>
 Span<Edge<BoardSize>>
 CachedRobot<BoardSize, SubRobotType, HashSize>::BestCandidateEdges(const ScoreCountableBoard<BoardSize>& board) {
-  size_t hash = board.Hash();
-  RobotCache<BoardSize>& CacheList = GlobalCache[hash % HashSize];
+  RobotCache<BoardSize>& CacheList = GlobalCache[board.Hash() % HashSize];
 
   Span<Edge<BoardSize>> result;
-  CacheList.Find(hash, result);
+  CacheList.Find(board, result);
   if (result.Empty()) {
     result = SubRobot.BestCandidateEdges(board);
-    CacheList.Add(hash, result);
+    CacheList.Add(board, result);
   }
 
   return result;
