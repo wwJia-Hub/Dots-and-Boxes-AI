411296aeef349355f5b938dd55d0150c2fb52c5f
wwJia <wwJia-Hub@users.noreply.github.com>
2026-02-02 20:01:09 +0800

    refactor(robot): simplify cache implementation in CachedRobot
    Replace complex atomic counters and shared mutexes with a simpler RobotCache class
    Remove unused includes and optimize cache storage structure

--- src/robot/CachedRobot.hpp
+++ src/robot/CachedRobot.hpp
@@ -1,12 +1,8 @@
 #pragma once
 
-#include <algorithm>
-#include <atomic>
 #include <cstddef>
 #include <cstdio>
 #include <mutex>
-#include <shared_mutex>
-#include <utility>
 #include <vector>
 
 #include "Board.hpp"
@@ -15,7 +11,48 @@
 
 namespace dab::detail::robot {
 
-template <int64_t BoardSize, typename SubRobotType, size_t HashSize = 1 << 12>
+template <int64_t BoardSize>
+class RobotCacheEntry {
+  public:
+  RobotCacheEntry() = default;
+  RobotCacheEntry(const BasicBoard<BoardSize>& board, const std::vector<Edge<BoardSize>>& result)
+      : Board(board), Result(result) {
+  }
+
+  BasicBoard<BoardSize> Board;
+  std::vector<Edge<BoardSize>> Result;
+};
+
+template <int64_t BoardSize>
+class RobotCache {
+  public:
+  RobotCache() {
+    Entrise.reserve(Edge<BoardSize>::Max);
+  }
+
+  void
+  Find(const BasicBoard<BoardSize>& board, Span<Edge<BoardSize>>& result) {
+    for (size_t i = 0; i < Entrise.size(); i++) {
+      RobotCacheEntry<BoardSize>& entry = Entrise[i];
+      if (entry.Board == board) {
+        result = Span(entry.Result.data(), entry.Result.data() + entry.Result.size());
+      }
+    }
+  }
+
+  void
+  Add(const BasicBoard<BoardSize>& board, const Span<Edge<BoardSize>>& result) {
+    std::vector<Edge<BoardSize>> resultDump(result.Begin(), result.End());
+    std::lock_guard lock(Mutex);
+    Entrise.emplace_back(board, resultDump);
+  }
+
+  private:
+  std::mutex Mutex;
+  std::vector<RobotCacheEntry<BoardSize>> Entrise;
+};
+
+template <int64_t BoardSize, typename SubRobotType, size_t HashSize = 1 << 14>
 class CachedRobot : public Robot<BoardSize> {
   public:
   CachedRobot() = default;
@@ -26,56 +63,22 @@ class CachedRobot : public Robot<BoardSize> {
   private:
   SubRobotType SubRobot;
 
-  static inline std::atomic<int> CachedCount = 0;
-  static inline std::atomic<int> TotalCount = 0;
-
-  static std::vector<std::vector<std::pair<BasicBoard<BoardSize>, std::vector<Edge<BoardSize>>>>>
-  CreateCache();
-  static inline std::vector<std::vector<std::pair<BasicBoard<BoardSize>, std::vector<Edge<BoardSize>>>>> GlobalCache =
-      CreateCache();
-  static inline Array<std::shared_mutex, HashSize> GlobalCacheMutex;
+  static inline std::vector<RobotCache<BoardSize>> GlobalCache = std::vector<RobotCache<BoardSize>>(HashSize);
 };
 
 template <int64_t BoardSize, typename SubRobotType, size_t HashSize>
 Span<Edge<BoardSize>>
 CachedRobot<BoardSize, SubRobotType, HashSize>::BestCandidateEdges(const ScoreCountableBoard<BoardSize>& board) {
-  TotalCount++;
-  if (TotalCount % 100000 == 0) {
-    size_t MaxSize = 0;
-    for (size_t i = 0; i < HashSize; i++) {
-      MaxSize = std::max(MaxSize, GlobalCache[i].size());
-    }
+  RobotCache<BoardSize>& CacheList = GlobalCache[board.Hash() % HashSize];
 
-    printf("[CachedRobot] Cached:Total %d:%d MaxSize %lu\n", CachedCount.load(), TotalCount.load(), MaxSize);
+  Span<Edge<BoardSize>> result;
+  CacheList.Find(board, result);
+  if (result.Empty()) {
+    result = SubRobot.BestCandidateEdges(board);
+    CacheList.Add(board, result);
   }
 
-  size_t HashValue = board.Hash();
-  size_t HashOffset = HashValue % HashSize;
-
-  std::vector<std::pair<BasicBoard<BoardSize>, std::vector<Edge<BoardSize>>>>& CacheList = GlobalCache[HashOffset];
-  for (const std::pair<BasicBoard<BoardSize>, std::vector<Edge<BoardSize>>>& entry : CacheList) {
-    if (entry.first == static_cast<BasicBoard<BoardSize>>(board)) {
-      CachedCount++;
-      return Span(entry.second.data(), entry.second.data() + entry.second.size());
-    }
-  }
-
-  Span<Edge<BoardSize>> result = SubRobot.BestCandidateEdges(board);
-  std::vector<Edge<BoardSize>> resultDump(result.begin(), result.end());
-
-  std::unique_lock lock(GlobalCacheMutex[HashOffset]);
-  CacheList.emplace_back(static_cast<BasicBoard<BoardSize>>(board), resultDump);
   return result;
-}
-
-template <int64_t BoardSize, typename SubRobotType, size_t HashSize>
-std::vector<std::vector<std::pair<BasicBoard<BoardSize>, std::vector<Edge<BoardSize>>>>>
-CachedRobot<BoardSize, SubRobotType, HashSize>::CreateCache() {
-  std::vector<std::vector<std::pair<BasicBoard<BoardSize>, std::vector<Edge<BoardSize>>>>> Cache(HashSize);
-  for (std::vector<std::pair<BasicBoard<BoardSize>, std::vector<Edge<BoardSize>>>>& arr : Cache) {
-    arr.reserve(Edge<BoardSize>::Max);
-  }
-  return Cache;
 }
 
 }  // namespace dab::detail::robot
