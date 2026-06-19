9f8cd36642e00eae79b407634eeaf8b145e17529
wwJia <wwJia-Hub@users.noreply.github.com>
2026-06-20 07:53:43 +0800

    chore: remove cmake flag

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 git submodule update --init --recursive
-cmake -B build -S . -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DCMAKE_BUILD_TYPE=${1:-Debug}
+cmake -B build -S . -DCMAKE_BUILD_TYPE=${1:-Debug}
 ln -sf build/compile_commands.json compile_commands.json
 cmake --build build -j $(nproc) --target Dots_and_Boxes
