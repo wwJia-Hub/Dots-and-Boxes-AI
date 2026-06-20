8877fce2a54de3ce4f27b7ab681565cc3f68f932
wwJia <wwJia-Hub@users.noreply.github.com>
2026-06-20 10:15:47 +0800

    ci: use parallel to replace nproc

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 git submodule update --init --recursive
 cmake -B build -S . -DCMAKE_BUILD_TYPE=${1:-Debug}
 ln -sf build/compile_commands.json compile_commands.json
-cmake --build build -j $(nproc) --target Dots_and_Boxes
+cmake --build build --parallel --target Dots_and_Boxes
